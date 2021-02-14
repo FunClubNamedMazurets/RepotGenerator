66c319188abea0572ef5371c6c0b12233b22b210
Oleskandr Ovchruk <cohik2000conik@gmail.com>
2021-02-14 20:46:29 +0200

    Revert "Revert "formaZvt""
    This reverts commit 91da980cc26d4b76f003e4f7c739623ab4250bea.

---
+++ ReportGenerator/Data/Entities/FormaZvt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGenerator.Data.Entities
+{
+    public class FormaZvt : BaseEntity
+    {
+        //назва організації профспілки
+        public string NameOfGuild { get; set; }
+
+        //за 20____ рік дві останні цифри
+        public string Year { get; set; }
+
+        //Технічних (головних технічних) інспекторів праці, всього:
+        public string NumberTechnicalInspectors { get; set; }
+
+        //Технічний інспектор праці
+        public string TechnicalInspector { get; set; }
+
+        public class Grid
+        {
+
+            //Кількість за періоди звітний
+            public string Column3 { get; set; }
+
+            //Кількість за періоди попередній
+            public string Column4 { get; set; }
+
+        }
+
+
+    }
+}
