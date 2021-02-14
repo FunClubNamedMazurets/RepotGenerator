fdc91c7c434c4d988d9daee2cb2985c8886025f7
Oleskandr Ovchruk <cohik2000conik@gmail.com>
2021-02-14 20:15:35 +0200

    formaZvt

---
+++ ReportGenerator/Generators/FormaZvtGenerator.cs
@@ -0,0 +1,43 @@
+﻿using ReportGenerator.Data.Entities;
+using ReportGenerator.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGenerator.Generators
+{
+    public class FormaZvtGenerator : BaseGenerator, IGenerator<FormaZvt>
+    {
+        private string Word { get; set; }
+
+        private FormaZvtService formaZvtService;
+
+        public FormaZvtGenerator(string path) : base(path)
+        {
+            formaZvtService = new FormaZvtService();
+
+        }
+
+        public void Generate(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(FormaZvt model)
+        {
+            formaZvtService.Insert(model);
+        }
+
+        public bool Validate(FormaZvt model)
+        {
+            if (model == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ReportGenerator/Services/FormaZvtService.cs
@@ -0,0 +1,57 @@
+﻿using ReportGenerator.Data.Contexts;
+using ReportGenerator.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGenerator.Services
+{
+    class FormaZvtService : IBaseService<FormaZvt>
+    {
+        private ReportContext _reportContext;
+
+        public FormaZvtService()
+        {
+            _reportContext = new ReportContext();
+        }
+
+        public void Delete(int id)
+        {
+            var formaZvts = _reportContext.FormaZvts.First(x => x.Id == id);
+
+            if (formaZvts != null)
+            {
+                _reportContext.FormaZvts.Remove(formaZvts);
+                _reportContext.SaveChanges();
+            }
+        }
+
+        public IList<FormaZvt> GetAll()
+        {
+            return _reportContext.FormaZvts.ToList();
+        }
+
+        public FormaZvt GetById(int id)
+        {
+            return _reportContext.FormaZvts.Where(x => x.Id == id).First();
+        }
+
+        public void Insert(FormaZvt entity)
+        {
+            _reportContext.FormaZvts.Add(entity);
+            _reportContext.SaveChanges();
+        }
+
+        public void Update(FormaZvt entity)
+        {
+            var formaZvts = _reportContext.FormaZvts.First(x => x.Id == entity.Id);
+            if (formaZvts != null)
+            {
+                formaZvts = entity;
+                _reportContext.SaveChanges();
+            }
+        }
+    }
+}
