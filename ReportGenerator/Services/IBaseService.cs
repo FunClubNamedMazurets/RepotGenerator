5c07b4c6e44bc97e038a8ff3c8858ac6afdd26f4
Faludore <monsterbanik@gmail.com>
2021-02-14 18:56:30 +0200

    rewrite service

---
+++ ReportGenerator/Services/IBaseService.cs
@@ -0,0 +1,22 @@
+﻿using ReportGenerator.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGenerator.Services
+{
+    public interface IBaseService<T> where T : BaseEntity
+    {
+        T GetById(int id);
+
+        IList<T> GetAll();
+
+        void Insert(T entity);
+
+        void Update(T entity);
+
+        void Delete(int id);
+    }
+}
